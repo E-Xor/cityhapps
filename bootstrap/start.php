bf33426d30aa6574b2ba0b884c70b6ca938745e8
Craig Kahle <craig@crispyinteractive.com>
2014-11-03 23:19:35 -0600

    start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array("cpk.local")
+	// 'local' => array("cpk.local")
 	// 'local' => array("NowlinCrispyMacbookAir.local")
 
 ));
