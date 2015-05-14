3723d55922f0b481b22235361cf95bed611517a5
bmax <bmax1337@gmail.com>
2015-05-13 21:22:44 -0400

    Added a check to see if table is empty or not, uses sources instead of urls now.

--- app/commands/StaleApiData.php
+++ app/commands/StaleApiData.php
@@ -6,43 +6,46 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class StaleApiData extends Command {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'api:clear-stale';
+  /**
+   * The console command name.
+   *
+   * @var string
+   */
+  protected $name = 'api:clear-stale';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Clears stale API data from main events table.';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Clears stale API data from main events table.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function fire()
+  {
     // output some log info
     $this->line("Start time: " . (string)date('l jS \of F Y h:i:s A'));
-    $this->line("Truncating all events that are not in API Tables (Eventbrite, Eventful, Meetup) and events that are in the past"); 
+    $this->line("Truncating all events that are not in API Tables (Eventbrite, Eventful, Meetup) and events that are in the past");
 
     // This finds the url's that are in events but not in any of the other tables! then deletes them
     $query = EventRecord::clearStaleApi();
-    $this->info("Deleted " . $query . " row(s) from events table");
-	}
+    if ($query == -1)
+      $this->info("Looks like the API Tables are clear! (No stale data to check against)");
+    else
+      $this->info("Deleted " . $query . " row(s) from events table");
+  }
 }
