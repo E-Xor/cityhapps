4cd4e7f353486d7e43d97b993d4d13e64b974806
Dan Hansen <dhansen@sevaa.com>
2015-07-13 10:57:08 -0400

    Tweaking migration to work

--- app/database/migrations/2015_07_05_143511_add_extra_venue_columns_eventbrite.php
+++ app/database/migrations/2015_07_05_143511_add_extra_venue_columns_eventbrite.php
@@ -14,20 +14,11 @@ class AddExtraVenueColumnsEventbrite extends Migration {
 	{
 		Schema::table('eventbrite', function(Blueprint $table)
 		{
-			//Drop old definition, add them add again in their correct positions, and using the correct data type
-			$table->dropColumn('latitude');
-			$table->dropColumn('longitude');
-		});
-
-		Schema::table('eventbrite', function(Blueprint $table)
-		{
 			$table->float('latitude', 16, 14)->nullable()->after('postal_code');
 			$table->float('longitude', 16, 14)->nullable()->after('latitude');
-			$table->integer('venue_id')->unsigned()->nullable()->after('venue_name');
-
 		});
 
-        Schema::table('events', function(Blueprint $table)
+        Schema::table('happs', function(Blueprint $table)
         {
             //Composite Key to avoid dup venues
             $table->unique(array('source', 'source_id'), 'SOURCE_ID_UNIQUE');
@@ -45,10 +36,9 @@ class AddExtraVenueColumnsEventbrite extends Migration {
 		{
 			$table->dropColumn('latitude');
 			$table->dropColumn('longitude');
-			$table->dropColumn('venue_id');
 		});
 
-        Schema::table('events', function(Blueprint $table){
+        Schema::table('happs', function(Blueprint $table){
             $table->dropIndex('SOURCE_ID_UNIQUE');
         });
 	}
