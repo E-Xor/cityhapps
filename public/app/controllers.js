5fb67328171ad3776478c8bfbab316f6234023f3
zaali kavelashvili <zkavalashvili@sevaa.com>
2015-10-09 23:40:59 +0400

    Angular datetimepicker configured

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -456,6 +456,33 @@ angular.module('cityHapps.controllers', []).controller('AuthController', functio
         }
 }).controller('adminEventController', function($scope, $http, $stateParams, $cookies, $cookieStore, Happ, AgeLevel, cleanData, $filter, Category) {
 
+    $scope.toggleMinDate = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.maxDate = new Date('2014-06-22');
+    $scope.toggleMinDate();
+    $scope.dateOptions = {
+      startingDay: 1,
+      showWeeks: false
+    };
+    // Disable weekend selection
+    $scope.disabled = function(calendarDate, mode) {
+      return mode === 'day' && ( calendarDate.getDay() === 0 || calendarDate.getDay() === 6 );
+    };
+    $scope.hourStep = 1;
+    $scope.minuteStep = 15;
+    $scope.timeOptions = {
+      hourStep: [1, 2, 3],
+      minuteStep: [1, 5, 10, 15, 25, 30]
+    };
+    $scope.showMeridian = true;
+    $scope.timeToggleMode = function() {
+      $scope.showMeridian = !$scope.showMeridian;
+    };
+    $scope.resetHours = function() {
+      $scope.date.setHours(1);
+    };
+
     $scope.user = $cookies.user;
     $scope.formData = {};
     AgeLevel.query(function(payload) {
