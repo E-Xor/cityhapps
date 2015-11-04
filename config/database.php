ed5e3209551e44bb287d60226767d15f8040a11e
Dan Hansen <dhansen@sevaa.com>
2015-11-03 23:52:16 -0500

    Removing unneeded changes

--- config/database.php
+++ config/database.php
@@ -55,7 +55,6 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'port'      => 8889,
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
@@ -63,7 +62,6 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-            'unix_socket'    => '/Applications/MAMP/tmp/mysql/mysql.sock',
         ],
 
         'pgsql' => [
