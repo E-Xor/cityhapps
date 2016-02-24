6ebd0fb67e48d89cbd74604770775f1ae99a458e
Matthew Machuga <machuga@gmail.com>
2016-02-24 07:13:25 -0500

    Adapting event form to use new styles
    Had to overhaul all markup and change most styles.  Broke into other
    sass files.

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('style.scss', 'public/assets/css/style.css');
+  mix.sass(['admin-form.scss','style.scss'], 'public/assets/css/style.css');
 });
