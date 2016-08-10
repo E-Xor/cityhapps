982cd87a15ce048fe31c0817add35b95703e0532
E-Xor <e-xormail@tut.by>
2016-08-10 09:16:31 -0400

    Facebook registration fix

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -89,8 +89,8 @@ class SessionsController extends Controller {
 		Log::Info('SessionsController::FacebookCallback');
 		Log::Info('SessionsController::FacebookCallback. Auth::check(): '. \Auth::check());
 
-		$user = Socialite::driver('facebook')->user();
-		// $user dump
+		$fbUser = Socialite::driver('facebook')->user();
+		// var_export($user, true);
 		// Laravel\Socialite\Two\User::__set_state(array(
 		//    'token' => 'EAAGa0D4ZBs5EBAPcvch1BoM4GV9rXMStVFMZB6KsljiTHBLM8O0IZALF1FZCn4YNq9TnXjkKKmhT33aA4MNXZBhgQn7yzcgGeQekzLL78LpE9HZB1rZAjIZA9lUZCISUZCBtETpLRwym7Nyd8l8fOnQtWyAgFBZAbdC7pkZD',
 		//    'refreshToken' => NULL,
@@ -111,15 +111,29 @@ class SessionsController extends Controller {
 		//    'avatar_original' => 'https://graph.facebook.com/v2.6/10210442562406023/picture?width=1920',
 		// ))
 
-		$token = $user->token;
+		$token = $fbUser->token;
 		Log::info('SessionsController::FacebookCallback. Token: ' . $token);
 
-		$name = $user->getName();
+		$name = $fbUser->getName();
 		Log::Info('SessionsController::FacebookCallback. Name: ' . $name);
-		$email = $user->getEmail();
+		$email = $fbUser->getEmail();
 		Log::Info('SessionsController::FacebookCallback. EMail: ' . $email);
 
 		$user = User::where('email', '=', $email)->first();
+
+		if(!$user) {
+			Log::Info('User not found. Register.');
+
+			$user = new User;
+			$user->email = $email;
+			$user->user_name = $name;
+			$user->role = User::ROLE_USER;
+			$user->city = 'Atlanta , GA';
+			$user->password = \Hash::make($token);
+
+			$user->save();
+		}
+
 		\Auth::login($user, true);
 
 		Log::Info("SessionsController::FacebookCallback. Auth::check(): ". \Auth::check());
